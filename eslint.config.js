cdd622f7445cbb59d2f15fb3e383b827e471e58f
Nathan Drake <nathandrakedrums@gmail.com>
2025-08-27 20:50:53 -0500

    initial lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,9 @@
 import { eslintConfig } from "js-style-kit";
 
 export default eslintConfig({
+  ignores: ["next-env.d.ts"],
   react: {
     framework: "next",
   },
+  typescript: "tsconfig.eslint.json",
 });
