da174f89da414147b025eac0e6a83e91d97d07dc
Nathan Drake <nathandrakedrums@gmail.com>
2025-08-27 20:52:57 -0500

    finish lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
@@ -100,4 +100,6 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
